2b23419374e950b9b994120f4853ba9637b4aefd
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-04-30 18:07:05 -0300

    Add login page

---
+++ src/main/java/com/solvd/carina/demo/gui/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.solvd.carina.demo.gui.pages;
+
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends AbstractPage {
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+}
