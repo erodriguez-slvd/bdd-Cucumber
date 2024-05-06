51d4f4c528acc19fb31d9b6ad43dba958af5c983
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-05-06 16:39:37 -0300

    Add mybatis config file

--- src/main/java/com/solvd/carina/demo/db/mappers/IUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.solvd.carina.demo.db.mappers;
-
-import com.solvd.carina.demo.db.models.User;
-
-public interface IUserMapper {
-    User findById(long id);
-    User findByUserName(String username);
-}
