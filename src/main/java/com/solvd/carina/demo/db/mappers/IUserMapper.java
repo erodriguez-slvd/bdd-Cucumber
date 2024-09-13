88aeeda8f5e396c3f806bcc9afc155f7077e86b7
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-09-13 17:32:13 -0300

    Corrections: Add CRUD operations: create, update, delete

--- src/main/java/com/solvd/carina/demo/db/mappers/IUserMapper.java
+++ src/main/java/com/solvd/carina/demo/db/mappers/IUserMapper.java
@@ -5,4 +5,7 @@ import com.solvd.carina.demo.db.models.User;
 public interface IUserMapper {
     User findById(long id);
     User findByUserName(String username);
+    void create(User user);
+    void update (User user);
+    void delete(long id);
 }
